6eee959e892e2b1759bd07e5cde917a2315a626c
NOMAN1802 <mustakimnoman82@gmail.com>
2023-07-04 03:46:25 +0600

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,13 +10,13 @@ import Container from '../Container/Container';
 const Home = () => {
     return (
         <Container>
-            <div className="flex my-8 mx-4">
-                <div className='w-1/2 font-bold my-4'>
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 my-8 mx-4">
+                <div className='font-bold my-4'>
                     <div className='Hero' id='home'>
                         <div className="wrapper">
                             <img src={portImage} alt="" />
                             <div className="typed">
-                                <h1 style={{fontFamily: 'cursive', fontSize: '20px'}} className='my-4'>I am</h1>
+                                <h1 style={{fontFamily: 'cursive', fontSize: '30px'}} className='my-4'>I am</h1>
                                 <span style={{fontFamily: 'cursive', fontSize: '20px'}}><Typed strings={[' Mustakim Al Noman', 'A React JS Developer']} typeSpeed={100} backSpeed={30} loop /></span>
                             </div>
 
@@ -26,7 +26,7 @@ const Home = () => {
 
                 </div>
 
-                <div className='w-1/2 my-8'>
+                <div className='my-8'>
                     <Lottie animationData={about} loop={true} />
 
                 </div>
